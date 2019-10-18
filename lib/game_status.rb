e595c1ea91a56bf03227633832efac9e27dd1666
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-18 17:04:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,9 +47,9 @@ end
 
 def winner(board)
   if winning_combo = won?(board)
-   winning_combo[0] == board[ winning_combo[0]]
+   winning_combo == board[ winning_combo[0]]
     return "X"
-  else winning_combo[1] == board[ winning_combo[1]]
+  else winning_combo == board[ winning_combo[1]]
     return "O"
   end
 end
